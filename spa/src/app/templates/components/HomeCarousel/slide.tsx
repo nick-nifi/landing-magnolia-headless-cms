bc61cd9714436737047917f7e0f948f4e7fb6097
tunv <nguyenvantu191298@gmail.com>
2025-11-21 19:55:08 +0700

    feat: Add generic carousel UI component and reimplement HomeCarousel using `embla-carousel-react`.

---
+++ spa/src/app/templates/components/HomeCarousel/slide.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+'use client';
+
+import { Grid } from '@/components/grid';
+import { Typography } from '@/components/typography';
+import Image from 'next/image';
+
+interface CarouselImage {
+  imageSrc: string;
+  imageAlt: string;
+  title: string;
+  description: string;
+}
+
+export interface SlideProps extends CarouselImage {}
+
+export default function Slide({
+  description,
+  imageAlt,
+  imageSrc,
+  title,
+}: SlideProps) {
+  return (
+    <div className='h-screen relative lg:h-[600px]'>
+      <div
+        className={`
+          absolute inset-0
+          z-5
+          bg-[linear-gradient(180deg,rgba(0,0,0,0)_0%,rgba(0,0,0,0.5)_59.24%)]
+          sm:bg-[linear-gradient(270deg,rgba(0,0,0,0)_7.42%,rgba(0,0,0,0.5)_82.03%)]
+          lg:bg-[linear-gradient(270deg,rgba(0,0,0,0)_24.03%,rgba(0,0,0,0.5)_87.05%)]
+          transition-all duration-500
+        `}
+        style={{ zIndex: 5 }}
+      />
+      <div
+        className='absolute top-0 left-0 z-3 w-full h-full'
+        style={{ zIndex: 3 }}
+      >
+        <div className='relative w-full h-full'>
+          <Image
+            src={imageSrc || ''}
+            alt={imageAlt || ''}
+            fill
+            className='object-cover z-1'
+            unoptimized
+          />
+        </div>
+      </div>
+
+      {/* {media && (
+        <div className='absolute top-0 left-0 z-3 w-full h-full'>
+          <div className='relative w-full h-full'>
+            {media.type === 'image' ? (
+              <Image
+                src={media.src || ''}
+                alt={title || ''}
+                fill
+                className='object-cover'
+                unoptimized
+              />
+            ) : (
+              <video
+                muted
+                src={media.src || ''}
+                autoPlay
+                poster={media.poster || ''}
+                loop
+                className='w-full h-full object-cover'
+              />
+            )}
+          </div>
+        </div>
+      )} */}
+
+      <div className='container px-2 lg:px-0 relative z-10 mx-auto h-full'>
+        <Grid mdCols={2} cols={1} className='h-full'>
+          <Grid className='h-full justify-center items-center flex'>
+            <div className='flex flex-col gap-8'>
+              {title && (
+                <Typography variant={'hero-heading'}>{title}</Typography>
+              )}
+              {description && (
+                <Typography variant={'body-large'} weight={'light'}>
+                  {description}
+                </Typography>
+              )}
+
+              {/* {actionLabel && (
+                <div>
+                  {actionHref ? (
+                    <Link href={actionHref}>
+                      <Button variant={'outline'} color='white'>
+                        {actionLabel} <ArrowRight />
+                      </Button>
+                    </Link>
+                  ) : (
+                    <Button variant={'outline'} color='white'>
+                      {actionLabel} <ArrowRight />
+                    </Button>
+                  )}
+                </div>
+              )} */}
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
