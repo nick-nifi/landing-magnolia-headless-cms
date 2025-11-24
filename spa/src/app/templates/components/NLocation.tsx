0852129677c6215041701c57911354bdbc475dbe
tunv <nguyenvantu191298@gmail.com>
2025-11-24 19:49:22 +0700

    feat: Add new Select UI component and redesign NLocation to display detailed location information with an embedded map.

--- spa/src/app/templates/components/NLocation.tsx
+++ spa/src/app/templates/components/NLocation.tsx
@@ -1,83 +1,101 @@
-import React from 'react';
-import { environment } from '../../../environments/environment';
-import { decodeIfEscaped } from '../../services/content-service';
 import { Grid } from '@/components/grid';
+import { Typography } from '@/components/typography';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import Link from 'next/link';
+import React from 'react';
 
-interface ImageChooser {
-  field?: 'image' | 'externalImage';
-  image?: {
-    '@link': string;
-  };
-  imageAlt?: string;
+interface INLocationProps {
+  company: string;
+  address: string;
+  tel?: string;
+  email?: string;
   externalImage?: string;
   externalImageAlt?: string;
 }
 
 interface INLocationProps {
-  title: string;
-  description: string;
-  imageChooser?: ImageChooser;
+  company: string;
+  address: string;
+  tel?: string;
+  email?: string;
+  // imageChooser?: ImageChooser;
 }
 
 const NLocation: React.FC<INLocationProps> = ({
-  title,
-  description,
-  imageChooser,
+  address,
+  company,
+  email,
+  tel,
+  // imageChooser,
 }) => {
-  let imageSrc = '';
-  let imageAlt = 'Image';
-
-  if (
-    imageChooser &&
-    imageChooser.field &&
-    (imageChooser.image || imageChooser.externalImage)
-  ) {
-    if (imageChooser.field === 'image' && imageChooser.image) {
-      imageSrc = `${environment.damRawBase}${imageChooser.image['@link']}`;
-      imageAlt = imageChooser.imageAlt || 'Image';
-    } else if (
-      imageChooser.field === 'externalImage' &&
-      imageChooser.externalImage
-    ) {
-      imageSrc = imageChooser.externalImage;
-      imageAlt = imageChooser.externalImageAlt || 'Image';
-    }
-  }
-
   return (
-    <section className='container mx-auto px-2 lg:px-0'>
-      <Grid cols={1} lgCols={3}>
-        <Grid>Lorem</Grid>
-        <Grid className='lg:col-span-2'>Lorem</Grid>
+    <section className='container mx-auto px-2 lg:px-0 py-16'>
+      <Grid cols={1} lgCols={3} gap={8}>
+        <Grid>
+          <Grid cols={1} mdCols={2} lgCols={1} gap={8}>
+            <Grid gap={8}>
+              <Typography variant='h2' weight={'light'}>
+                Global branches
+              </Typography>
+              <Select defaultValue='2' value='2'>
+                <SelectTrigger className='w-full'>
+                  <SelectValue>Lorem</SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value='2'>Singapore head office</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </Grid>
+            <Grid>
+              <div>
+                <Typography
+                  variant={'body-large'}
+                  weight={'semibold'}
+                  className='text-primary'
+                >
+                  {company}
+                </Typography>
+                <Typography variant={'body-large'} weight={'light'}>
+                  {address}
+                </Typography>
+                {tel && (
+                  <Typography variant={'body-large'} weight={'light'}>
+                    Tel: {tel}
+                  </Typography>
+                )}
+                {email && (
+                  <Typography variant={'body-large'} weight={'light'}>
+                    <Link href={`mailto:${email}`}>Email: {email}</Link>
+                  </Typography>
+                )}
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid className='lg:col-span-2'>
+          <div className='embed-map-responsive w-full h-full min-h-[500px] lg:min-h-[200px]'>
+            <div className='embed-map-container w-full h-full'>
+              <iframe
+                className='embed-map-frame w-full h-full'
+                title='Google Map'
+                src='https://maps.google.com/maps?width=600&height=400&hl=en&q=83%20Clemenceau%20Avenue%2C%20%2310-01%20UE%20Square%2C%20Singapore%20239920&t=p&z=14&ie=UTF8&iwloc=B&output=embed'
+                frameBorder='0'
+                scrolling='no'
+              ></iframe>
+            </div>
+          </div>
+        </Grid>
       </Grid>
     </section>
-  );
-  return (
-    <div className='w-full p-4'>
-      <div className='rounded overflow-hidden shadow-lg bg-white'>
-        {imageSrc ? (
-          <div className='w-full h-48 overflow-hidden'>
-            <img
-              src={imageSrc}
-              alt={imageAlt}
-              className='w-full h-full object-cover object-center'
-            />
-          </div>
-        ) : (
-          <div className='w-full h-48 flex items-center justify-center bg-gray-200'>
-            <p>No image available.</p>
-          </div>
-        )}
-        <div className='p-6'>
-          {title && <h2 className='font-bold text-xl mb-2'>{title}</h2>}
-          {description && (
-            <div
-              dangerouslySetInnerHTML={{ __html: decodeIfEscaped(description) }}
-            />
-          )}
-        </div>
-      </div>
-    </div>
   );
 };
 
