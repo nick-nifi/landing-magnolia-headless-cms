8fbbfc7f788b5ab2395a103da918cfd68ad9c349
tunv <nguyenvantu191298@gmail.com>
2025-12-01 15:58:53 +0700

    feat: add CardZ1 component with customizable title, description, and select field; update grid and basic templates to include CardZ1

---
+++ spa/src/app/templates/components/CardZ1.tsx
@@ -0,0 +1,39 @@
+import { IconName } from '@/components/icon-wrapper/register';
+import React from 'react';
+import { decodeIfEscaped } from '../../services/content-service';
+import IconWrapper from '@/components/icon-wrapper';
+import { Typography } from '@/components/typography';
+
+interface ICardZ1Props {
+  title: string;
+  description: string;
+  selectField: IconName;
+}
+
+const CardZ1: React.FC<ICardZ1Props> = ({
+  title,
+  description,
+  selectField: iconName,
+}) => {
+  return (
+    <div className='p-5 border shadow-md flex flex-col lg:flex-row'>
+      {iconName && (
+        <div className='w-[70px] h-[70px] lg:w-[98px] lg:h-[98px] flex items-center justify-center shrink-0'>
+          <IconWrapper name={iconName} width={50} height={50} />
+        </div>
+      )}
+      <div className='lg:px-5 lg:pb-6'>
+        <Typography variant={'h4'} className='mb-2'>
+          {title}
+        </Typography>
+        <Typography
+          variant={'body-large'}
+          weight={'light'}
+          dangerouslySetInnerHTML={{ __html: decodeIfEscaped(description) }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CardZ1;
--- spa/src/magnolia.config.ts
+++ spa/src/magnolia.config.ts
@@ -1,3 +1,4 @@
+import CardZ1Component from './app/templates/components/CardZ1'
 import ListD5Component from './app/templates/components/ListD5'
 import FastfactTileComponent from './app/templates/components/FastfactTile'
 import FlexibleC3Component from './app/templates/components/FlexibleC3';
@@ -58,6 +59,8 @@ export const config: MagnoliaConfig = {
     'spa-lm:components/textImage': TextImage,
     'spa-lm:components/typography': Typography,
     "spa-lm:components/fastfactTile": FastfactTileComponent,
-    "spa-lm:components/listD5": ListD5Component
+    "spa-lm:components/listD5": ListD5Component,
+    "spa-lm:components/cardZ1": CardZ1Component
+    
   },
 };
