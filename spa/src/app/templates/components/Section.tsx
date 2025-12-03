e3c60f4f635e7282afed9114c0b58c8ffb152365
tunv <nguyenvantu191298@gmail.com>
2025-12-03 17:23:33 +0700

    refactor: replace container div and specific padding with `neo-container` class on Section component.

--- spa/src/app/templates/components/Section.tsx
+++ spa/src/app/templates/components/Section.tsx
@@ -10,10 +10,8 @@ interface ISectionProps {
 
 const Section: React.FC<ISectionProps> = ({ content, customClass }) => {
   return (
-    <section className={cn('py-12 md:py-16 lg:py-28', customClass)}>
-      <div className='container mx-auto px-2 lg:px-0'>
-        {content && <EditableArea content={content} />}
-      </div>
+    <section className={cn('neo-container', customClass)}>
+      {content && <EditableArea content={content} />}
     </section>
   );
 };
