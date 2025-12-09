5bf1fe30dc1f3492de26753adac40b3d2d81d8a7
tunv <nguyenvantu191298@gmail.com>
2025-12-10 01:31:44 +0700

    feat: Implement HeaderA2 component with dynamic content, CTA, and styling, while renaming title/description properties and adding z-index utilities.

--- spa/src/app/templates/pages/components/HeaderA2.tsx
+++ spa/src/app/templates/pages/components/HeaderA2.tsx
@@ -1,12 +1,22 @@
+'use client';
 import { decodeIfEscaped } from '@/app/services/content-service';
 import { Typography } from '@/components/typography';
+import { Button } from '@/components/ui/button';
 import { environment } from '@/environments/environment';
+import { ArrowRight, ExternalLink } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRef } from 'react';
 
 export interface HeaderA2Item {
-  Title: string;
-  Description: string;
+  title: string;
+  description: string;
   image?: string;
+  ctaChooser?: {
+    label: string;
+    isExternal: boolean;
+    href: string;
+  };
 }
 
 interface HeaderA2Props {
@@ -18,9 +28,78 @@ export default function HeaderA2({ content }: HeaderA2Props) {
     ? `${environment.damRawBase}/magnoliaAuthor/dam/${content.image}`
     : '';
 
+  const sectionRef = useRef<HTMLElement>(null);
+
   if (!content) return null;
 
-  return <section data-name='header a2'></section>;
+  return (
+    <section
+      ref={sectionRef}
+      data-name='header a2'
+      className='md:h-[460px] bg-gray-200 relative'
+      style={{
+        background: 'linear-gradient(101.22deg, #C33B32 1.75%, #D55C4D 58.27%)',
+      }}
+    >
+      <div data-name='content-group' className='w-full h-full z-[3]'>
+        <div className='container h-full'>
+          <div className='grid grid-cols-1 md:grid-cols-2 h-full'>
+            <div className='h-full flex flex-col justify-center text-white gap-4'>
+              <Typography variant={'h1'} weight='light'>
+                {content.title}
+              </Typography>
+              {content.description && (
+                <Typography
+                  variant={'body-large'}
+                  weight='light'
+                  className='pr-4 line-clamp-3'
+                  dangerouslySetInnerHTML={{
+                    __html: decodeIfEscaped(content.description),
+                  }}
+                />
+              )}
+              {content.ctaChooser?.label && (
+                <div>
+                  <Button asChild variant={'outline'} color='white'>
+                    <Link href={content.ctaChooser.href || '#'}>
+                      {content.ctaChooser.label}
+                      {content.ctaChooser.isExternal ? (
+                        <ExternalLink />
+                      ) : (
+                        <ArrowRight />
+                      )}
+                    </Link>
+                  </Button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        data-name='mask-group'
+        className='absolute inset-0 w-full h-full z-[2] pointer-events-none'
+      ></div>
+
+      {imageUrl && (
+        <div
+          data-name='image-group'
+          className='absolute inset-0 w-full h-full z-[1] flex grid md:grid-cols-2 pointer-events-none'
+        >
+          <div />
+          <div className='relative w-full h-full'>
+            <Image
+              fill
+              unoptimized
+              src={imageUrl}
+              alt={content.title || 'Header Image'}
+              className='object-cover'
+            />
+          </div>
+        </div>
+      )}
+    </section>
+  );
 
   // return (
   //   <div className='relative w-full h-[300px] mb-8'>
